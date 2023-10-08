ed2980e9c598f0c8a9c0cee027df585c8a576e34
Savio-Anjos <savioanjos2020@gmail.com>
2023-10-07 22:58:49 -0300

    :sparkles: feat: Prisma configuration created

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: "Savio Anjos",
+    email: "Savio@teste.com",
+  },
+});
 
 export const app = fastify();
